42d777e861a4ebc2bbfb5a13a0afe966f5471d20
kuuku123 <dohyun0047@gmail.com>
2021-03-23 20:06:19 +0900

    fetch Lazy (X to One) On , Cascade All ON , addxxx for OOP ON

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -16,14 +16,14 @@ public class Order
     @Id @GeneratedValue
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id")
     private Member member;
 
-    @OneToMany(mappedBy = "order")
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
     private List<OrderItem> orderItems = new ArrayList<>();
 
-    @OneToOne
+    @OneToOne(fetch = FetchType.LAZY,cascade = CascadeType.ALL)
     @JoinColumn(name = "delivery_id")
     private Delivery delivery;
 
@@ -35,6 +35,17 @@ public class Order
     {
         this.member = member;
         member.getOrders().add(this);
+    }
 
+    public void addOrderItem(OrderItem orderItem)
+    {
+        this.orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+
+    public void addDelivery(Delivery delivery)
+    {
+        this.delivery = delivery;
+        delivery.setOrder(this);
     }
 }
