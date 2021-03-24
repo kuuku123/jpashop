54e73a0ac2ac6c3d339d97479efa6b513e112c97
kuuku123 <dohyun0047@gmail.com>
2021-03-24 16:10:43 +0900

    item business logic implemented , NotEnoughStockException implemented

--- src/main/java/jpabook/jpashop/domain/Item/Item.java
+++ src/main/java/jpabook/jpashop/domain/Item/Item.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.domain.Item;
 
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -24,4 +25,29 @@ public abstract class Item
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    /**
+     * increase stock
+     */
+    public void addStock(int quantity)
+    {
+        this.stockQuantity += quantity;
+    }
+
+    /**
+     * decrease stock
+     */
+    public void removeStock(int quantity)
+    {
+        int restStock = this.stockQuantity - quantity;
+        if (restStock<0)
+        {
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
+
+
+
+
 }
