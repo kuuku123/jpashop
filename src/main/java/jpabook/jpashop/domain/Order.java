bebd5e3433e55af562b4a88f90061d7beaeb793f
kuuku123 <dohyun0047@gmail.com>
2021-03-24 17:46:20 +0900

    Order ,OrderItem business logic and create instance method implemented

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -31,7 +31,7 @@ public class Order
 
     private OrderStatus orderStatus;
 
-    public void addMember(Member member)
+    public void setMember(Member member)
     {
         this.member = member;
         member.getOrders().add(this);
@@ -43,9 +43,51 @@ public class Order
         orderItem.setOrder(this);
     }
 
-    public void addDelivery(Delivery delivery)
+    public void setDelivery(Delivery delivery)
     {
         this.delivery = delivery;
         delivery.setOrder(this);
+    }
+
+
+    //==create instance==//
+    public static Order createOrder(Member member, Delivery delivery, OrderItem... orderItems)
+    {
+        Order order = new Order();
+        order.setMember(member);
+        order.setDelivery(delivery);
+        for (OrderItem orderItem : orderItems)
+        {
+            order.addOrderItem(orderItem);
+        }
+        order.setOrderStatus(OrderStatus.ORDER);
+        order.setOrderDate(LocalDateTime.now());
+        return order;
+    }
+
+    //==business logic==//
+    /**
+     * cancel order
+     */
+    public void cancel()
+    {
+        if (delivery.getDeliveryStatus() == DeliveryStatus.COMP)
+        {
+            throw new IllegalStateException("이미 배송완료된 상품은 취소가 불가능합니다.");
+        }
+
+        this.setOrderStatus(OrderStatus.CANCEL);
+        for (OrderItem orderItem : orderItems)
+        {
+            orderItem.cancel();
+        }
+    }
+
+    /**
+     * total cost
+     */
+    public int getTotalPrice()
+    {
+        return orderItems.stream().mapToInt(OrderItem::getTotalPrice).sum();
     }
 }
--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -25,4 +25,27 @@ public class OrderItem
     private int orderPrice;
     private int count;
 
+    //==create instance==//
+    public static OrderItem createOrderItem(Item item,int orderPrice,int count)
+    {
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
+
+        item.removeStock(count);
+        return orderItem;
+    }
+
+
+    //==business logic==//
+    public void cancel()
+    {
+        getItem().addStock(count);
+    }
+
+    public int getTotalPrice()
+    {
+        return getOrderPrice()*getCount();
+    }
 }
